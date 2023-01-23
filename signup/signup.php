d6e99354f052c9a9a0cf24008ce65993c4d98694
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-01-23 14:36:52 +0100

    Beginning signup.php

--- signup/signup.php
+++ signup/signup.php
@@ -1,1 +1,11 @@
 <?php
+
+require("/src/models/conn.php");
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+
+$cif_pass = password_hash($password,PASSWORD_DEFAULT);
+
+//$consulta = "SELECT * FROM "
