55b07406221ca2e653a5f976ba7b189dc3429a72
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-01-25 14:01:03 +0100

    Removing unused variables and reformating

--- dashboard/api/index.php
+++ dashboard/api/index.php
@@ -1,15 +1,10 @@
 <?php
 session_start();
-require "" . $_SERVER['DOCUMENT_ROOT'] . "/imapi/src/models/conn.php";
 $mail = $_SESSION['correo'];
 $consulta = "SELECT * FROM imgs";
-$registros = mysqli_query($conection, $consulta)
-  or die("Problemas" . mysqli_error($conection));
-$reg = mysqli_fetch_array($registros);
-$nombre = $reg['nombre'];
-$apellidos = $reg['apellidos'];
-$myfile = fopen("index.json", "w");
-$txt = json_encode($reg);
-fwrite($myfile, $txt);
-fclose($myfile);
+require $_SERVER['DOCUMENT_ROOT'] . "/imapi/src/models/consulta.php";
+$archivo = fopen("index.json", "w");
+$json = json_encode($reg);
+fwrite($archivo, $json);
+fclose($archivo);
 header("location: /imapi/dashboard/api/index.json");
