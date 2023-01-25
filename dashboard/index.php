5385b45d5e1977e2189e510cba686cd01e7efb5d
oussama02 <oussamaosman02@gmail.com>
2023-01-25 18:26:21 +0100

    Dashboard styles

--- dashboard/index.php
+++ dashboard/index.php
@@ -5,12 +5,15 @@ if (empty($_SESSION['correo'])) {
 }
 ?>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App</title>
+    <link rel="stylesheet" href="../src/styles/dashboard.module.css" />
 </head>
+
 <body>
     <?php
     $mail = $_SESSION['correo'];
@@ -20,27 +23,40 @@ if (empty($_SESSION['correo'])) {
     $registros = mysqli_query($conection, $consulta)
         or die("Problemas" . mysqli_error($conection));
     $reg = mysqli_fetch_array($registros);
-    echo "Nombre: " . ($reg['nombre']);
-    echo "Apellidos: " . ($reg['apellidos']);
-    echo "Mail: " . ($reg['mail']);
-    echo "<img src='https://api.dicebear.com/5.x/avataaars/svg?seed=" . $reg['nombre'] . "' width='200' alt=''>";
     ?>
-    <a href="/imapi/logout">
-        <button>Cerrar Sesión</button>
-    </a>
-    <a href="/imapi/dashboard/profile">
-        <button>Ir al perfil</button>
-    </a>
-
-    <?php
-       $conection = new mysqli("localhost", "root", "", "api");
-       $conection->set_charset("utf8");
-       $consulta = "SELECT * FROM imgs";
-       $registros = mysqli_query($conection, $consulta)
-       or die("Problemas" . mysqli_error($conection));
-   while ($fila = $registros->fetch_assoc()) {
-       echo "<img src='https://api.dicebear.com/5.x/avataaars/svg?seed=" . $fila['imagen'] . "' width='".$fila['width']."' alt=''>";
-    }
-    ?>
+    <nav>
+        <div>
+            <img src="../public/android-chrome-192x192.png" alt="imagen" />
+            <h2>IMAPI x <?php echo ($reg['nombre']); ?></h2>
+        </div>
+        <ul>
+            <li>
+                <a href="/imapi/logout">Cerrar Sesión</a>
+            </li>
+            <li>
+                <a href="/imapi/dashboard/profile">Perfil</a>
+            </li>
+        </ul>
+        <div class="avatar"><?php echo "<img src='https://api.dicebear.com/5.x/avataaars/svg?seed=" . $reg['nombre'] . "' width='200' alt=''>";
+                            ?> </div>
+    </nav>
+    <h1>Imágenes disponibles</h1>
+    <div class="img">
+        <?php
+        $conection = new mysqli("localhost", "root", "", "api");
+        $conection->set_charset("utf8");
+        $consulta = "SELECT * FROM imgs where tipo='gratis'";
+        $registros = mysqli_query($conection, $consulta)
+            or die("Problemas" . mysqli_error($conection));
+        while ($fila = $registros->fetch_assoc()) {
+            $imagen = $fila['imagen'];
+            $width = $fila['width'];
+            $height = $fila['height'];
+            echo "<img src='https://api.dicebear.com/5.x/avataaars/svg?seed=$imagen'  style='--w:$width ;--h: $height;' alt='imagen de $imagen' />";
+        }
+        ?>
+        <img src="" alt="">
+    </div>
 </body>
+
 </html>
