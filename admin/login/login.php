d16050f59888368e0c1596437b6be3ef37a3bced
oussama02 <oussamaosman02@gmail.com>
2023-01-25 19:46:46 +0100

    LogIn admin

---
+++ admin/login/login.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+$mail = $_POST['mail'];
+$password = $_POST['contra'];
+
+$conection = new mysqli("localhost", "root", "", "api");
+$conection->set_charset("utf8");
+
+$consulta = "SELECT * FROM admins WHERE mail='$mail'";
+
+$registros = mysqli_query($conection, $consulta)
+    or die("Problemas" . mysqli_error($conection));
+
+if ($reg = mysqli_fetch_array($registros)) {
+    if (password_verify($password, $reg['passwd'])) {
+        $_SESSION['correo'] = $mail;
+        header("location: /imapi/admin");
+    } else {
+        echo "<script>alert('Error con la contraseña'); history.back();</script>";
+    }
+} else {
+    echo "<script>alert('Correo equivocado'); history.back();</script>";
+}
