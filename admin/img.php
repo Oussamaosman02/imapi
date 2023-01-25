4372d576c140f4c301da1b6921ffa90749ef55af
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-01-25 09:15:07 +0100

    Refactoring

--- admin/img.php
+++ admin/img.php
@@ -1,20 +1,14 @@
 <?php
-
-
 $width = $_POST['width'];
 $height = $_POST['height'];
 $imagen = $_POST['imagen'];
 $ext = $_POST['extension'];
 $tipo = $_POST['tipo'];
-
 $conection = new mysqli("localhost", "root", "", "api");
 $conection->set_charset("utf8");
-
 $consulta = "SELECT * FROM imgs WHERE imagen='$imagen'";
-
 $registros = mysqli_query($conection, $consulta)
     or die("Problemas" . mysqli_error($conection));
-
 if ($reg = mysqli_fetch_array($registros)) {
     echo "<script>alert('El correo ya existe'); history.back();</script>";
 } else {
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,29 +1,22 @@
 <?php
 session_start();
-
 if (empty($_SESSION['correo'])) {
     header("location: /imapi/login");
 }
 ?>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App</title>
 </head>
-
 <body>
     <?php
-
-
     $mail = $_SESSION['correo'];
     $conection = new mysqli("localhost", "root", "", "api");
     $conection->set_charset("utf8");
-
     $consulta = "SELECT * FROM users WHERE mail='$mail'";
-
     $registros = mysqli_query($conection, $consulta)
         or die("Problemas" . mysqli_error($conection));
     $reg = mysqli_fetch_array($registros);
@@ -36,5 +29,4 @@ if (empty($_SESSION['correo'])) {
         <button>Cerrar Sesión</button>
     </a>
 </body>
-
 </html>
