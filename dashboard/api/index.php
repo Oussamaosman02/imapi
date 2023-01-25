1af432fe5f7ffed7a1037f90c97871364e47ab05
oussama02 <oussamaosman02@gmail.com>
2023-01-25 20:40:00 +0100

    Updating json creation

--- dashboard/api/index.php
+++ dashboard/api/index.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
 $mail = $_SESSION['correo'];
-$consulta = "SELECT * FROM imgs";
+$consulta = "SELECT * FROM imgs where tipo='gratis'";
 require $_SERVER['DOCUMENT_ROOT'] . "/imapi/src/models/consulta.php";
 $archivo = fopen("index.json", "w");
-$json = json_encode($reg);
+while ($fila = $registros->fetch_assoc()) {
+    $data[] = $fila;
+}
+$json = json_encode($data);
 fwrite($archivo, $json);
 fclose($archivo);
 header("location: /imapi/dashboard/api/index.json");
