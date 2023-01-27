e271ae8e99c9cf7a63660498d2946f304333c744
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-01-27 10:02:34 +0100

    Adding first object to json

--- dashboard/api/index.php
+++ dashboard/api/index.php
@@ -3,6 +3,7 @@ session_start();
 $mail = $_SESSION['correo'];
 $consulta = "SELECT * FROM imgs where tipo='gratis'";
 require $_SERVER['DOCUMENT_ROOT'] . "/imapi/src/models/consulta.php";
+$data[]=$reg;
 $archivo = fopen("index.json", "w");
 while ($fila = $registros->fetch_assoc()) {
     $data[] = $fila;
